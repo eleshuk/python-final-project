8c829e70355492ce59f64693d4d380f309a712d8
Elizabeth Leshuk <eleshuk@gmail.com>
2025-01-15 14:38:15 +0000

    calculate quickstats and printin terminal

--- weather_analysis/precipitation_analysis.py
+++ weather_analysis/precipitation_analysis.py
@@ -9,16 +9,16 @@ def precipitation_data_avg(data):
 
     # Get the number of days from timedelta
     num_days = date_range.days
-    print("Date range is: " + str(num_days) + " days")
+    # print("Date range is: " + str(num_days) + " days")
 
     if num_days <= 14:
-        print("Window is 3")
+        # print("Window is 3")
         rolling_average = data['Precipitation'].rolling(window=3, min_periods=1).mean()
     elif num_days > 14 and num_days <=60:
-        print("Window is 7")
+        # print("Window is 7")
         rolling_average = data['Precipitation'].rolling(window=7, min_periods=1).mean()
     elif num_days > 60:
-        print("Window is 30")
+        # print("Window is 30")
         rolling_average = data['Precipitation'].rolling(window=30, min_periods=1).mean()
     
     # ra_df = pd.DataFrame(rolling_average)
@@ -39,9 +39,13 @@ def precipitation_quick_stats(data):
     day_most_rain = data.loc[data['Precipitation'].idxmax(), 'Date']
     day_least_rain = data.loc[data['Precipitation'].idxmin(), 'Date']
         # Return results as a dictionary
-    return {
+    stats =  {
         'max_precipitation': max_precipitation,
         'min_precipitation': min_precipitation,
         'day_most_rain': day_most_rain,
         'day_least_rain': day_least_rain
     }
+    stats_df = pd.DataFrame([stats])
+    print("Precipitation quick stats:")
+    print(stats_df)
+
